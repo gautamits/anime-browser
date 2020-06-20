cece7c2c6cf76113235af4bec42bdc6b75439d8d
amit <gautamamits95@gmail.com>
2020-06-20 22:52:42 +0530

    Updates

---
+++ precache-manifest.e827c9bb89db761bf09c9f4dd296af35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb29e5cb361d31907f7f7db19801234",
+    "url": "/anime-browser/index.html"
+  },
+  {
+    "revision": "5ddfe5179471a147fda5",
+    "url": "/anime-browser/static/css/main.6b01e749.chunk.css"
+  },
+  {
+    "revision": "274cea49e917ab3c69a4",
+    "url": "/anime-browser/static/js/2.20bf979c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/anime-browser/static/js/2.20bf979c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ddfe5179471a147fda5",
+    "url": "/anime-browser/static/js/main.ac50676b.chunk.js"
+  },
+  {
+    "revision": "074f72e1f679baad32fc",
+    "url": "/anime-browser/static/js/runtime-main.5a4aa2e9.js"
+  }
+]);
